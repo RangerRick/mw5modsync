304d8db9c5bfa2299fd09231013d9d72e4277309
Benjamin Reed <github@raccoonfink.com>
2023-02-13 21:48:13 -0500

    fix running from explorer

--- do-sync.bat
+++ do-sync.bat
@@ -1,1 +1,4 @@
+REM if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
+
+cd "%~dp0"
 %SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy RemoteSigned .\mw5-sync.ps1
