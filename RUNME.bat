b9870b3730a1a26fbe155ad786142f3138c054cf
Benjamin Reed <git@raccoonfink.com>
2023-03-02 16:21:33 -0500

    stay open if the job fails

--- RUNME.bat
+++ RUNME.bat
@@ -1,4 +1,4 @@
-REM if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
+if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
 cd "%~dp0"
 %SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy RemoteSigned .\mw5-sync.ps1
